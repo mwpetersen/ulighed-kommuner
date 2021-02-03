cde6993ef01ae3a4f76392256c465f83d2987a7a
Mikkel Wittenburg Petersen <mikkel.w.petersen@gmail.com>
2021-02-03 15:44:24 +0100

    added requests

--- scripts/2020-02-03-import-data-load-postgres.py
+++ scripts/2020-02-03-import-data-load-postgres.py
@@ -12,6 +12,84 @@ from pyjstat import pyjstat
 url_dst = 'https://api.statbank.dk/v1/data'
 
 ## Parameters for the API
+query_indkomst_kommuner = {
+   "table": "IFOR32",
+   "format": "JSONSTAT",
+   "valuePresentation": "CodeAndValue",
+   "variables": [
+      {
+         "code": "DECILGEN",
+         "values": [
+            "*"
+         ]
+      },
+      {
+         "code": "KOMMUNEDK",
+         "values": [
+            "*"
+         ]
+      },
+      {
+         "code": "Tid",
+         "values": [
+            "(-n+10)"
+         ]
+      }
+   ]
+}
+
+query_pct_lavindkomst_kommuner = {
+   "table": "IFOR12P",
+   "format": "JSONSTAT",
+   "valuePresentation": "CodeAndValue",
+   "variables": [
+      {
+         "code": "KOMMUNEDK",
+         "values": [
+            "*"
+         ]
+      },
+      {
+         "code": "INDKN",
+         "values": [
+            "50"
+         ]
+      },
+      {
+         "code": "Tid",
+         "values": [
+            "(-n+10)"
+         ]
+      }
+   ]
+}
+
+query_n_lavindkomst_kommuner = {
+   "table": "IFOR12A",
+   "format": "JSONSTAT",
+   "valuePresentation": "CodeAndValue",
+   "variables": [
+      {
+         "code": "KOMMUNEDK",
+         "values": [
+            "*"
+         ]
+      },
+      {
+         "code": "INDKN",
+         "values": [
+            "50"
+         ]
+      },
+      {
+         "code": "Tid",
+         "values": [
+            "(-n+10)"
+         ]
+      }
+   ]
+}
+
 query_folketal = {
    "table": "FOLK1A",
    "format": "JSONSTAT",
@@ -39,12 +117,30 @@ query_folketal = {
 }
 
 ## Post query
-res_folketal = requests.post(url_dst, json = query_folketal)
+r_indkomst_kommuner = requests.post(url_dst, json = query_indkomst_kommuner)
+
+r_pct_lavindkomst_kommuner = requests.post(url_dst, json = query_pct_lavindkomst_kommuner)
+
+r_n_lavindkomst_kommuner = requests.post(url_dst, json = query_n_lavindkomst_kommuner)
+
+r_folketal = requests.post(url_dst, json = query_folketal)
 
 ## Read JSON-stat result
-ds_folketal = pyjstat.Dataset.read(res_folketal.text)
+ds_indkomst_kommuner = pyjstat.Dataset.read(r_indkomst_kommuner.text)
+
+ds_pct_lavindkomst_kommuner = pyjstat.Dataset.read(r_pct_lavindkomst_kommuner.text)
+
+ds_n_lavindkomst_kommuner = pyjstat.Dataset.read(r_n_lavindkomst_kommuner.text)
+
+ds_folketal = pyjstat.Dataset.read(r_folketal.text)
 
 ## Write to pandas dataframe
-df_folketal = ds_folketal.write('dataframe')
+df_indkomst_kommuner = ds_indkomst_kommuner.write('dataframe', naming = 'id')
 
+df_pct_lavindkomst_kommuner = ds_pct_lavindkomst_kommuner.write('dataframe', naming = 'id')
 
+df_n_lavindkomst_kommuner = ds_n_lavindkomst_kommuner.write('dataframe', naming = 'id')
+
+df_folketal_tekst = ds_folketal.write('dataframe', naming = 'label')
+df_folketal_kode = ds_folketal.write('dataframe', naming = 'id')
+df_folketal_tekst['kommune_id'] = df_folketal_kode['OMRÅDE']
