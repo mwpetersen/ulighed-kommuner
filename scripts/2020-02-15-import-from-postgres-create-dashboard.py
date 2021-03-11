82afc5c40a2f0d6008830cc9d435c09c072ce135
Mikkel Wittenburg Petersen <mikkel.w.petersen@gmail.com>
2021-03-11 10:59:39 +0100

    added second line plot, and bar plot

--- scripts/2020-02-15-import-from-postgres-create-dashboard.py
+++ scripts/2020-02-15-import-from-postgres-create-dashboard.py
@@ -102,7 +102,6 @@ fig_indkomst = px.line(
   x = "år",
   y = "g_indkomst",
   color = "decil_gruppe",
-  hover_name = "kommune_navn",
   custom_data=["decil_gruppe", "år", "g_indkomst"])
   )
 
@@ -120,12 +119,17 @@ fig_indkomst = px.line(
 # style line plot
 x = np.sort(df_g_indkomst_kbh['år'].unique()).tolist()
 
+## values to get x axis length - used in range argument in .update_layout
+x_min = df_g_indkomst_kbh['år'].min() - 0.1
+x_max = df_g_indkomst_kbh['år'].max() + 0.1
+
 fig_indkomst.update_layout(
     xaxis=dict(
         showline=True,
         showgrid=False,
         showticklabels=True,
         tickvals=x,
+        range = [x_min,x_max],
         linecolor='rgb(204, 204, 204)',
         linewidth=2,
         ticks='outside',
@@ -168,6 +172,7 @@ fig_indkomst.update_layout(
     )
 )
 
+# Set line color and width, and set information in tooltip
 fig_indkomst.update_traces(
   line=dict(color='rgb(39,112,214)', width=2),
   hovertemplate="Gruppe: %{customdata[0]} <br>År: %{customdata[1]} </br>Indkomst: %{customdata[2]}")
@@ -211,6 +216,185 @@ fig_indkomst.show()
 ## linjegraf med andel af befolkningen de sidste 10 år, der lever i lavindkomstfamilier 
 ## (antal mennesker i tooltip)
 
+df_kommuner_g_lavindkomst = (df_kommuner_g_lavindkomst
+  .merge(df_kommuner, left_on = 'kommune_id', right_on = 'id', how = 'left')
+  .drop(['kommune_id', 'id'], axis = 1)
+  )
+
+kbh = df_kommuner_g_lavindkomst['kommune_navn'] == 'København'
+
+df_g_lavindkomst_kbh = df_kommuner_g_lavindkomst[(kbh)]
+
+## Create plot
+
+fig_lavindkomst = px.line(
+  df_g_lavindkomst_kbh,
+  x = "år",
+  y = "p_lavindkomst",
+  hover_name = "kommune_navn",
+  custom_data=["lavindkomst_niveau", "år", "p_lavindkomst", "n_lavindkomst", "kommune_navn"],
+  height=400
+  )
+
+fig_lavindkomst.update_layout(
+    xaxis=dict(
+        showline=True,
+        showgrid=False,
+        showticklabels=True,
+        tickvals=x,
+        range = [x_min,x_max],
+        linecolor='rgb(204, 204, 204)',
+        linewidth=2,
+        ticks='outside',
+        tickfont=dict(
+            family='Arial',
+            size=12,
+            color='rgb(82, 82, 82)',
+        ),
+    ),
+    yaxis=dict(
+        range = [0, 23],
+        showline=True,
+        showgrid=False,
+        showticklabels=True,
+        showticksuffix='all',
+        ticksuffix='%', # https://plotly.com/javascript/tick-formatting/
+        linecolor='rgb(204, 204, 204)',
+        linewidth=2,
+        ticks='outside',
+        tickfont=dict(
+            family='Arial',
+            size=12,
+            color='rgb(82, 82, 82)',
+        ),
+    ),
+    autosize=False,
+    margin=dict(
+        autoexpand=False,
+        l=100,
+        r=50,
+        t=110,
+    ),
+    xaxis_title=None,
+    yaxis_title=None,
+    plot_bgcolor='white',
+    hoverlabel=dict(
+        bgcolor="white",
+        font_size=12,
+        font_family='Arial'
+    )
+)
+
+fig_lavindkomst.update_traces(
+  line=dict(color='rgb(39,112,214)', width=4),
+  hovertemplate=("</br><b>%{customdata[4]}</b></br>"
+                 "</br>År: %{customdata[1]}</br>" + 
+                 "Andel: %{customdata[2]} procent</br>" +
+                 "Antal: %{customdata[3]}</br>" +
+                 "Lavindkomstniveau: %{customdata[0]} procent af medianen"))
+
+
+annotations_low = []
+
+# Add title
+annotations_low.append(dict(xref='paper', yref='paper', x=0.0, y=1.05,
+                              xanchor='left', yanchor='bottom',
+                              text='Andel (i %) af befolkningen de sidste 10 år, der lever i en lavindkomstfamilie',
+                              font=dict(family='Arial',
+                                        size=16,
+                                        color='rgb(37,37,37)'),
+                              showarrow=False))
+
+# Add source
+annotations_low.append(dict(xref='paper', yref='paper', x=1.0, y=-0.15,
+                              xanchor='right', yanchor='top',
+                              text='Kilde: Danmarks Statistik',
+                              font=dict(family='Arial',
+                                        size=12,
+                                        color='rgb(150,150,150)'),
+                              showarrow=False))
+
+fig_lavindkomst.update_layout(
+  annotations=annotations_low)
+
+fig_lavindkomst.show()
+
 ## søjlegraf med top fem kommuner det seneste opgørelsesår med den største andel af 
 ## deres befolkning der lever i lavindkomstfamilier
 
+seneste_år = df_kommuner_g_lavindkomst['år'].max()
+
+lavindkomst_top5 = (df_kommuner_g_lavindkomst
+  .loc[df_kommuner_g_lavindkomst['år'] == seneste_år]
+  .nlargest(5, 'p_lavindkomst', keep = 'all')
+)
+
+fig_top5 = px.bar(
+  lavindkomst_top5,
+  x = 'p_lavindkomst',
+  y = 'kommune_navn',
+  text='p_lavindkomst' + '%',
+  orientation = 'h',
+  height=350
+)
+
+fig_top5.update_traces(marker_color='rgb(39,112,214)')
+
+fig_top5.update_layout(
+    xaxis=dict(
+        showline=False,
+        showgrid=False,
+        showticklabels=False
+    ),
+    yaxis=dict(
+      autorange="reversed",
+      tickfont=dict(
+            family='Arial',
+            size=12,
+            color='rgb(82, 82, 82)',
+        ),
+    ),
+    autosize=False,
+    margin=dict(
+        autoexpand=False,
+        l=100,
+        r=50,
+        t=110,
+        pad=10 # https://stackoverflow.com/questions/52391451/how-do-i-add-space-between-the-tick-labels-and-the-graph-in-plotly-python
+    ),
+    xaxis_title=None,
+    yaxis_title=None,
+    plot_bgcolor='white',
+    hoverlabel=dict(
+        bgcolor="white",
+        font_size=12,
+        font_family='Arial'
+    )
+)
+
+annotations_top5 = []
+
+# Add title
+annotations_top5.append(dict(xref='paper', yref='paper', x=0.0, y=1.05,
+                              xanchor='left', yanchor='bottom',
+                              text='De 5 kommuner med den største andel der lever i en lavindkomstfamilie',
+                              font=dict(family='Arial',
+                                        size=16,
+                                        color='rgb(37,37,37)'),
+                              showarrow=False))
+
+# Add source
+annotations_top5.append(dict(xref='paper', yref='paper', x=1.0, y=-0.05,
+                              xanchor='right', yanchor='top',
+                              text='Kilde: Danmarks Statistik',
+                              font=dict(family='Arial',
+                                        size=12,
+                                        color='rgb(150,150,150)'),
+                              showarrow=False))
+
+fig_top5.update_layout(
+  annotations=annotations_top5)
+
+fig_top5.update_traces(texttemplate='%{text} %')
+
+fig_top5.show()
