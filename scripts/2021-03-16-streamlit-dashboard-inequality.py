fca285761973adf6a3fca9b9af05b5dcdcb1f7fc
Mikkel Wittenburg Petersen <mikkel.w.petersen@gmail.com>
2021-03-17 09:55:55 +0100

    Updated script with queries to DST's API

--- scripts/2021-03-16-streamlit-dashboard-inequality.py
+++ scripts/2021-03-16-streamlit-dashboard-inequality.py
@@ -11,7 +11,7 @@ import streamlit as st
 
 # Import data from dst
 
-## URL til tabel med folketal
+## URL to DST's API
 url_dst = 'https://api.statbank.dk/v1/data'
 
 ## Parameters for the API
@@ -41,14 +41,112 @@ query_indkomst_kommuner = {
    ]
 }
 
+query_pct_lavindkomst_kommuner = {
+   "table": "IFOR12P",
+   "format": "JSONSTAT",
+   "valuePresentation": "CodeAndValue",
+   "variables": [
+      {
+         "code": "KOMMUNEDK",
+         "values": [
+            "*"
+         ]
+      },
+      {
+         "code": "INDKN",
+         "values": [
+            "50"
+         ]
+      },
+      {
+         "code": "Tid",
+         "values": [
+            "(-n+10)"
+         ]
+      }
+   ]
+}
+
+query_n_lavindkomst_kommuner = {
+   "table": "IFOR12A",
+   "format": "JSONSTAT",
+   "valuePresentation": "CodeAndValue",
+   "variables": [
+      {
+         "code": "KOMMUNEDK",
+         "values": [
+            "*"
+         ]
+      },
+      {
+         "code": "INDKN",
+         "values": [
+            "50"
+         ]
+      },
+      {
+         "code": "Tid",
+         "values": [
+            "(-n+10)"
+         ]
+      }
+   ]
+}
+
+query_folketal = {
+   "table": "FOLK1A",
+   "format": "JSONSTAT",
+   "valuePresentation": "CodeAndValue",
+   "variables": [
+      {
+         "code": "OMRÅDE",
+         "values": [
+            "*"
+         ]
+      },
+      {
+         "code": "Tid",
+         "values": [
+            "*K1"
+         ]
+      },
+      {
+         "code": "Tid",
+         "values": [
+            "(-n+10)"
+         ]
+      }
+   ]
+}
+
 ## Post query
 r_indkomst_kommuner = requests.post(url_dst, json = query_indkomst_kommuner)
+
+r_pct_lavindkomst_kommuner = requests.post(url_dst, json = query_pct_lavindkomst_kommuner)
+
+r_n_lavindkomst_kommuner = requests.post(url_dst, json = query_n_lavindkomst_kommuner)
+
+r_folketal = requests.post(url_dst, json = query_folketal)
 
 ## Read JSON-stat result
 ds_indkomst_kommuner = pyjstat.Dataset.read(r_indkomst_kommuner.text)
 
+ds_pct_lavindkomst_kommuner = pyjstat.Dataset.read(r_pct_lavindkomst_kommuner.text)
+
+ds_n_lavindkomst_kommuner = pyjstat.Dataset.read(r_n_lavindkomst_kommuner.text)
+
+ds_folketal = pyjstat.Dataset.read(r_folketal.text)
+
 ## Write to pandas dataframe
 df_indkomst_kommuner = ds_indkomst_kommuner.write('dataframe', naming = 'label')
+
+df_pct_lavindkomst_kommuner = ds_pct_lavindkomst_kommuner.write('dataframe', naming = 'id')
+
+df_n_lavindkomst_kommuner = ds_n_lavindkomst_kommuner.write('dataframe', naming = 'id')
+
+df_folketal_tekst = ds_folketal.write('dataframe', naming = 'label')
+df_folketal_kode = ds_folketal.write('dataframe', naming = 'id')
+df_folketal_tekst['id'] = df_folketal_kode['OMRÅDE']
 
 # Data cleaning and wrangling
 kun_kommuner = df_indkomst_kommuner["kommune"] != "Hele landet"
